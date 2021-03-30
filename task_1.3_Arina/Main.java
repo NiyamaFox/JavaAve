88862ca70ffa257b397ed2c572b5beafa6e8689d
Arina&Lesha <81516804+NiyamaFox@users.noreply.github.com>
2021-03-30 14:48:07 +0300

    Create Main.java

---
+++ task_1.3_Arina/Main.java
@@ -0,0 +1,10 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 9;
+        String s = "20";
+    }
+}
