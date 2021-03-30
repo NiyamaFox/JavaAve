a1df5b917bd5910edbd50de82e78d4b32f3dcfdd
Arina&Lesha <81516804+NiyamaFox@users.noreply.github.com>
2021-03-30 15:10:11 +0300

    Create Main.java

---
+++ task_1.6_Arina/Main.java
@@ -0,0 +1,11 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println(a + a * 0.15);
+
+    }
+}
